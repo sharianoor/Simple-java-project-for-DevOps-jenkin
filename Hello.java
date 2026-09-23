da7cb79f3e5d14da6c25c4f64d00c873abace261
Sharia Noor <sharianoor2@gmail.com>
2026-09-23 13:11:22 +0500

    Change added

--- Hello.java
+++ Hello.java
@@ -1,5 +1,6 @@
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Sharia Noor FA23-BDS-036");
+		System.out.println("Sharia Noor FA23-BDS-036 ");
+        System.out.println("devops ");
 	}
 }
